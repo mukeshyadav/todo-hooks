2dca314d6c7ff363c6ac4ac81c4484d166a10a99
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-01 22:28:50 +0530

    adding styles to app using tailwind css

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -3,16 +3,38 @@ import TodosContext from "../context.js";
 
 export default function TodoList() {
   const { state } = useContext(TodosContext);
+  const title =
+    state.todos.length > 0
+      ? `${state.todos.length} Todos`
+      : "Not to do, add one!";
 
   return (
-    <>
-      <ul>
+    <div className="container mx-auto max-w-md text-center font-mono">
+      <h1 className="text-bold">{title}</h1>
+      <ul className="list-rest text-white p-0">
         {state.todos.map(todo => (
-          <li key={todo.id}>
-            <span>{todo.text}</span>
+          <li
+            key={todo.id}
+            className="bg-orange-600 border-black border-dashed border-2 my-2 py-4 flex items-center"
+          >
+            <span className="cursor-pointer flex-1 ml-12">{todo.text}</span>
+            <button>
+              <img
+                src="https://icon.now.sh/edit/0050c5"
+                alt="Edit Todo"
+                className="h-6"
+              />
+            </button>
+            <button>
+              <img
+                src="https://icon.now.sh/delete/8b0000"
+                alt="Delete Todo"
+                className="h-6"
+              />
+            </button>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
