c4f4b6e06a1400e33bb3dcbdbcb1a087d78749ff
mukesh <mukesh_yadav@mckinsey.com>
2020-06-02 20:46:44 +0530

    toggle todos on dblclick

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,40 +1,40 @@
-import React, { useContext } from "react";
-import TodosContext from "../context.js";
+import React, { useContext } from 'react'
+import TodosContext from '../context.js'
 
-export default function TodoList() {
-  const { state } = useContext(TodosContext);
+export default function TodoList () {
+  const { state, dispatch } = useContext(TodosContext)
   const title =
-    state.todos.length > 0
-      ? `${state.todos.length} Todos`
-      : "Not to do, add one!";
+  state.todos.length > 0
+    ? `${state.todos.length} Todos`
+    : 'Not to do, add one!'
 
   return (
-    <div className="container mx-auto max-w-md text-center font-mono">
-      <h1 className="text-bold">{title}</h1>
-      <ul className="list-rest text-white p-0">
+    <div className='container mx-auto max-w-md text-center font-mono'>
+      <h1 className='text-bold'>{title}</h1>
+      <ul className='list-rest text-white p-0'>
         {state.todos.map(todo => (
-          <li
-            key={todo.id}
-            className="bg-orange-600 border-black border-dashed border-2 my-2 py-4 flex items-center"
-          >
-            <span className="cursor-pointer flex-1 ml-12">{todo.text}</span>
-            <button>
-              <img
-                src="https://icon.now.sh/edit/0050c5"
-                alt="Edit Todo"
-                className="h-6"
-              />
-            </button>
-            <button>
-              <img
-                src="https://icon.now.sh/delete/8b0000"
-                alt="Delete Todo"
-                className="h-6"
-              />
-            </button>
-          </li>
-        ))}
+                                                                                     <li
+                                                                                       key={todo.id}
+                                                                                       className="bg-orange-600 border-black border-dashed border-2 my-2 py-4 flex items-center"
+                                                                                     >
+                                                                                       <span className={`cursor-pointer flex-1 ml-12 ${todo.complete && "line-through text-gray-900"}`} onDoubleClick={() => dispatch({type: "TOGGLE_TODO", payload: todo})}>{todo.text}</span>
+                                                                                       <button>
+                                                                                         <img
+                                                                                           src="https://icon.now.sh/edit/0050c5"
+                                                                                           alt="Edit Todo"
+                                                                                           className="h-6"
+                                                                                         />
+                                                                                       </button>
+                                                                                       <button>
+                                                                                         <img
+                                                                                           src="https://icon.now.sh/delete/8b0000"
+                                                                                           alt="Delete Todo"
+                                                                                           className="h-6"
+                                                                                         />
+                                                                                       </button>
+                                                                                     </li>
+                                                                                   ))}
       </ul>
     </div>
-  );
+  )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,14 @@
-export default function reducer(state, action) {
+import { act } from "react-dom/test-utils"
+
+export default function reducer (state, action) {
   switch (action.type) {
+    case 'TOGGLE_TODO':
+      const toggleTodos = state.todos.map(t => t.id === action.payload.id ? {...action.payload, complete: !action.payload.complete} : t)
+      return {
+        ...state, todos: toggleTodos
+      }
+      break
     default:
-      return state;
+      return state
   }
 }
