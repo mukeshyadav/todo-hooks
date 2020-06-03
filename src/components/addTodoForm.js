af83e2c2d4045ddda86da2fc81009094a8e787b7
mukeshyadav <mukeshyadav@gmail.com>
2020-06-03 13:41:37 +0530

    recommit the changes

---
+++ src/components/addTodoForm.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useContext } from "react";
+import TodosContext from "../context.js";
+
+export default function AddTodoForm() {
+  const [todo, addTodo] = useState("");
+  const {
+    state: { currentTodo = {} },
+    dispatch,
+  } = useContext(TodosContext);
+
+  useEffect(() => {
+    if (currentTodo.text) {
+      addTodo(currentTodo.text);
+    }
+  }, [currentTodo.id]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (currentTodo.text) {
+      dispatch({ type: "UPDATE_TODO", payload: todo });
+    } else {
+      dispatch({ type: "ADD_TODO", payload: todo });
+    }
+    addTodo("");
+  };
+
+  return (
+    <form className="flex justify-center p-5" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="border-gray-600 border-solid border-2"
+        value={todo}
+        onChange={(event) => addTodo(event.target.value)}
+      />
+    </form>
+  );
+}
--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,59 @@
-import { act } from "react-dom/test-utils"
+import uuidv4 from "uuid/v4";
 
-export default function reducer (state, action) {
+export default function reducer(state, action) {
   switch (action.type) {
-    case 'TOGGLE_TODO':
-      const toggleTodos = state.todos.map(t => t.id === action.payload.id ? {...action.payload, complete: !action.payload.complete} : t)
+    case "TOGGLE_TODO":
+      const toggleTodos = state.todos.map((t) =>
+        t.id === action.payload.id
+          ? { ...action.payload, complete: !action.payload.complete }
+          : t
+      );
       return {
-        ...state, todos: toggleTodos
-      }
-      break
+        ...state,
+        todos: toggleTodos,
+      };
+
+    case "REMOVE_TODO":
+      const filterTodos = state.todos.filter((t) => t.id !== action.payload.id);
+      return {
+        ...state,
+        todos: filterTodos,
+      };
+
+    case "ADD_TODO":
+      const newTodo = {
+        id: uuidv4(),
+        text: action.payload,
+        complete: false,
+      };
+      const addedTodos = [...state.todos, newTodo];
+      return {
+        ...state,
+        todos: addedTodos,
+      };
+    case "SET_CURRENT_TODO":
+      return {
+        ...state,
+        currentTodo: action.payload,
+      };
+    case "UPDATE_TODO":
+      const updatedTodo = { ...state.currentTodo, text: action.payload };
+      const updatedTodoIndex = state.todos.findIndex(
+        (t) => t.id === state.currentTodo.id
+      );
+      const updatedTodos = [
+        ...state.todos.slice(0, updatedTodoIndex),
+        updatedTodo,
+        ...state.todos.slice(updatedTodoIndex + 1),
+      ];
+
+      return {
+        ...state,
+        currentTodo: {},
+        todos: updatedTodos,
+      };
+
     default:
-      return state
+      return state;
   }
 }
